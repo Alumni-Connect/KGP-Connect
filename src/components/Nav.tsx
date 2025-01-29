15b0cc1e09f1919845ef0e57c8b524d2159e9ca9
Rushil Vora <rushilvora1@gmail.com>
2025-01-30 00:26:30 +0530

    refactor: made the navbar look a little nicer

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,31 +5,32 @@
 
     export default function Navbar() {
         return (
-            <nav className="fixed w-full h-24 shadow-xl bg-white">
+            <nav className="fixed w-full shadow-xl bg-white">
                 <div className="flex justify-between items-center px-4">
                     <div>
                         <Link href='/'>
                         <Image src={kgpconnectLogo} alt="logo" width="205" height="205"></Image>
                         </Link>
                     </div>
-                    <div>
-                        <ul className="flex">
-                            <Link href='/about'>
-                                <li className="ml-10 uppercase hover:border-b text-xl text-black justify-center">About</li>
+                    <div className="flex items-center">
+                            <ul className="flex items-center">
+                                <Link href='/about'>
+                                    <li className="ml-10 uppercase hover:border-b text-xl text-black justify-center">About</li>
+                                </Link>
+                                <Link href='/contacts'>
+                                    <li className="ml-10 uppercase hover:border-b text-xl text-black justify-center">Contacts</li>
+                                </Link>
+                                <Link href='/home'>
+                                    <li className="ml-10 uppercase hover:border-b text-xl text-black justify-center">Home</li>
+                                </Link>
+                            </ul>
+                        <div className="ml-10">
+                            <Link href='/login'>
+                            <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Login</button>
                             </Link>
-                            <Link href='/contacts'>
-                                <li className="ml-10 uppercase hover:border-b text-xl text-black justify-center">Contacts</li>
-                            </Link>
-                            <Link href='/home'>
-                                <li className="ml-10 uppercase hover:border-b text-xl text-black justify-center">Home</li>
-                            </Link>
-                        </ul>
+                        </div>
                     </div>
-                    <div className="ml-10">
-                        <Link href='/login'>
-                        <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Login</button>
-                        </Link>
-                    </div>
+
                 </div>
             </nav>
         )
