806b45f5c138275954c9361275c9eb33ad259a5e
saranshgupta2711 <saranshkeshari9@gmail.com>
2025-01-30 01:00:32 +0530

    feat:verification token

---
+++ src/app/verify-email/page.tsx
@@ -0,0 +1,25 @@
+"use server"
+
+import { redirect } from "next/navigation"
+import { checkVerificationToken } from "../../../db/token"
+export default async function  CheckToken({ searchParams }: { searchParams: { token: string } }){
+
+    const token= searchParams.token
+     
+        if(!token){
+        console.log("no token provided")
+        return
+        }
+
+    const response=await checkVerificationToken(token)
+
+    if(response){
+        console.log(response)
+        redirect("/")
+    }else{
+        redirect("/login")
+    }
+    
+
+
+}
