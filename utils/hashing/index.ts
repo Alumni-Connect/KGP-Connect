4eda43b57f88865a7004c599ede5b2d130020f3b
Rushil Vora <rushilvora1@gmail.com>
2025-01-31 00:02:15 +0530

    feat:added schemas for donations and posts

--- utils/hashing/index.ts
+++ utils/hashing/index.ts
@@ -2,28 +2,28 @@ import bcrypt from "bcrypt"
 
 const salt=10
 export const hashPassword=async(password:string)=>{
-    try{
-    const hash=await bcrypt.hash(password,salt)
-    return {
-        status:true,
-       hashedPassword: hash
-    }
-    }catch(e){
-        return {
-            status:false
-            }
-    }
-  }
+   try{
+      const hash=await bcrypt.hash(password,salt)
+      return {
+         status:true,
+         hashedPassword: hash
+      }
+   }catch(e){
+      return {
+         status:false
+         }
+   }
+}
 
-  export const checkPassword=async(password:string,hashedPassword:string)=>{
-     try{
-        const check=await bcrypt.compare(password,hashedPassword)
-        if(!check){
-            return false
-        }
-        return true
-     }catch(e){
-        console.log(e)
-        return false
-     }
-  }
+export const checkPassword=async(password:string,hashedPassword:string)=>{
+   try{
+      const check=await bcrypt.compare(password,hashedPassword)
+      if(!check){
+         return false
+      }
+      return true
+   }catch(e){
+      console.log(e)
+      return false
+   }
+}
