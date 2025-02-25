e05881e457042ca01237d2de44be4371c0b60272
saranshgupta2711 <saranshkeshari9@gmail.com>
2025-02-25 13:09:34 +0530

    chore: schema migration removing

--- prisma/migrations/20250224204556_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "verification_Otp" (
-    "id" TEXT NOT NULL,
-    "identifier" TEXT NOT NULL,
-    "otp" TEXT NOT NULL,
-    "expires" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "verification_Otp_pkey" PRIMARY KEY ("id")
-);
