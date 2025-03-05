194c69f2549e078f173974b219788d2555e56a02
saranshgupta2711 <saranshkeshari9@gmail.com>
2025-03-05 21:58:26 +0530

    prisma defualt changed

--- src/app/api/user/admin/route.ts
+++ src/app/api/user/admin/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse,NextRequest } from "next/server";
 import { hashPassword } from "../../../../utils/hashing";
-import { prisma } from "../../../../lib/prisma";
+import { prisma }  from "@/lib/prisma";
 
 
 export async function POST(req:Request) {
