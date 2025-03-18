fa58ff1097691875f1b2ccad872aed70b95eecdb
uditangshu <145389897+uditangshu@users.noreply.github.com>
2025-03-18 13:38:53 +0530

    things

---
+++ src/app/jobboard/page.tsx
@@ -0,0 +1,37 @@
+import JobCard from "@/components/jobboard/JobCard";
+import { Job } from "@/types";
+import JobSearch from "@/components/jobboard/JobSearch";
+import Sidebar from "@/components/Sidebar";
+import { JSX } from "react";
+import Header from "../../components/Nav"
+
+export default async function JobBoard() {
+
+    const res = await fetch("http://localhost:3000/api/jobboard", {
+        method: "GET",
+    })
+    const jobs = await res.json();
+    console.log(jobs)
+
+    return (
+        <>
+            <div className="flex">
+                <Header></Header>
+                <div className="hidden lg:block w-60 min-w-[240px]">
+                    <Sidebar/>
+                </div>
+                <div className="mt-16 ml-8">
+                   
+                    <div className="flex justify-center">
+                        <div className="flex flex-wrap justify-start ml-7 gap-6 p-6">
+                            {jobs.map((job: JSX.IntrinsicAttributes & Job) => (
+                                <JobCard key={job.id} {...job} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    )
+}
