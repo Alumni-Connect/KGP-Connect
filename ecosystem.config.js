c453ba3d94e8899909665641b800cd2090c33441
saranshgupta2711 <saranshkeshari9@gmail.com>
2025-07-09 13:26:12 +0530

    addition of process management through pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name   : "Kgp adda app",
+    script: 'npm',
+    args: ['run', 'dev', '--', '--port=3001'],
+    max_memory_restart : '2G',
+    time : true,
+    shutdown_with_message : true,
+    max_restarts : 10,
+    restart_delay: 4000,
+  }]
+}
